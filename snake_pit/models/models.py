ddff7df911e907668cb84ee3564344817d7b7916
kzonca <keatonzonca@gmail.com>
2017-03-05 17:09:55 -0600

    Updated Models

--- snake_pit/models/models.py
+++ snake_pit/models/models.py
@@ -2,19 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
-class SurveyPrivacy(models.Model):
-    pType = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.pType
-
-class MemberType(models.Model):
-    mType = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.mType
-
 ##class User(models.Model):
 ##    memberType = models.ForeignKey(MemberType, on_delete=models.CASCADE,)
 ##    username = models.CharField(max_length=50, help_text="Username", unique=True)
@@ -24,7 +11,7 @@ class MemberType(models.Model):
 ##        return self.username
 
 class Survey(models.Model):
-    privacyID = models.ForeignKey(SurveyPrivacy, on_delete=models.CASCADE,)
+    private = models.BooleanField()
     title = models.CharField(max_length=200, help_text="Enter Survey Title")
     description = models.TextField(max_length=1000, help_text="Enter Survey Description")
     choices =  models.FileField(upload_to='uploads/survey_files', null=True)
@@ -41,6 +28,7 @@ class Result(models.Model):
         return self.userID + self.result
 
 class Pending(models.Model):
+    
     userID = models.ForeignKey(User, on_delete=models.CASCADE,)
     expire =  models.DateField()
     surveyID = models.ForeignKey(Survey, on_delete=models.CASCADE,)
@@ -50,9 +38,39 @@ class Pending(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    memberType = models.ForeignKey(MemberType, on_delete=models.CASCADE,)
-    race = models.CharField(max_length=20, help_text="Race", null=True, blank=True)
-    gender = models.CharField(max_length=20, help_text="Gender", null=True, blank=True)
+
+    RESEARCHER = "RESEARCHER"
+    TAKER = "SURVEY TAKER"
+    BOTH = "BOTH"
+    MTYPE_CHOICES = (
+    (RESEARCHER, "Researcher"),
+    (TAKER, "Survey Taker"),
+    (BOTH, "Both"),
+    )    
+    memberType = models.CharField(max_length=20, choices=MTYPE_CHOICES, help_text="Member Type",)
+
+    WHITE = "WHITE"
+    AM = "AFRICAN AMERICAN"
+    ASIAN = "ASIAN"
+    NA = "NATIVE AMERICAN"
+    PI = "PACIFIC ISLANDER"
+    RACE_CHOICES = (
+    (WHITE, "White"),
+    (AM, "African American"),
+    (ASIAN, "Asian"),
+    (NA, "Native American"),
+    (PI, "Pacific Islander"),
+    ) 
+    race = models.CharField(max_length=20, choices=RACE_CHOICES, help_text="Race", null=True, blank=True)
+
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    GENDER_CHOICES = (
+    (MALE, "Male"),
+    (FEMALE, "Female"),
+    ) 
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES, help_text="Gender", null=True, blank=True)
+
     country = models.CharField(max_length=50, help_text="Country", null=True, blank=True)
     city = models.CharField(max_length=50, help_text="City", null=True, blank=True)
     state = models.CharField(max_length=50, help_text="State", null=True, blank=True)
